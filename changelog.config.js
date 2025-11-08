889c5f0911708793a041642cb54317ce2a2a4fb2
vieerr <olivier_paspuel@hotmail.com>
2025-11-08 18:19:17 -0500

    ci(config): 🎡 add commitzen config with scopes

---
+++ changelog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+  scopes: [
+    'config',
+    'docs',
+    'users',
+    'animals',
+    'habitats',
+    'animals',
+    'caretakers',
+  ],
+  maxMessageLength: 100,
+}
