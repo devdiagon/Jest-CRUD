7f5e49f8a4468394307a2b1c63f73effd051f870
vieerr <olivier_paspuel@hotmail.com>
2025-11-17 22:10:31 -0500

    feat(zookeepers): 🎸 add zookeepers routes

--- src/routes/zookeeper.routes.js
+++ src/routes/zookeeper.routes.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const { getAllZookeepers, getZookeeperById, createZookeeper, updateZookeeper, deleteZookeeper } = require('../controllers/zookeeper.controller');
 
 const router = express.Router();
+
+router.get('/', getAllZookeepers);
+router.get('/:id', getZookeeperById);
+router.post('/', createZookeeper);
+router.put('/:id', updateZookeeper);
+router.delete('/:id', deleteZookeeper);
 
 module.exports = router;
