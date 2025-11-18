83f293847ed0bf5647dcae785786f0ac99419c9f
vieerr <olivier_paspuel@hotmail.com>
2025-11-17 22:10:15 -0500

    feat(habitats): 🎸 add habitat routes

--- src/routes/habitat.routes.js
+++ src/routes/habitat.routes.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const { getAllHabitats, getHabitatById, createHabitat, updateHabitat, deleteHabitat } = require('../controllers/habitat.controller');
 
 const router = express.Router();
+
+router.get('/', getAllHabitats);
+router.get('/:id', getHabitatById);
+router.post('/', createHabitat);
+router.put('/:id', updateHabitat);
+router.delete('/:id', deleteHabitat);
 
 module.exports = router;
